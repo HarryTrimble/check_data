a6aca9f909d790fcac31ae9787b6cd9aa93cadc0
Harry <harryroberttrimble@gmail.com>
2016-09-07 10:12:28 +0100

    additions to cancel code flow

--- app/routes.js
+++ app/routes.js
@@ -1,33 +1,30 @@
-var express = require('express')
-var router = express.Router()
+  var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+
+ // Store common vars
+
+ res.locals.formData = "";
+ res.locals.formQuery = "?";
+ res.locals.data = {};
+
+ for (var name in req.query){
+   var value = req.query[name];
+   res.locals.formData += '<input type="hidden" name="'+name+'" value="' + value + '">\n';
+   res.locals.formQuery += name + "=" + value + "&";
+   res.locals.data[name] = value;
+ }
+
+ res.locals.formQuery = res.locals.formQuery.slice(0,-1);
+
+ next();
+ 
+});
 
 router.get('/', function (req, res) {
-  res.render('index')
-})
+  res.render('index');
 
-// Example routes - feel free to delete these
+});
 
-// Passing data into a page
-
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name': 'Foo' })
-})
-
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18
-
-  if (over18 === 'false') {
-    // redirect to the relevant page
-    res.redirect('/examples/under-18')
-  } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18')
-  }
-})
-
-// add your routes here
-
-module.exports = router
+module.exports = router;
